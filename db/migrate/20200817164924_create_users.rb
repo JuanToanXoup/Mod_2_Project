a6edcd7c15624999286fb09b69947ff09ac40d7c
JuanToanXoup <Johndoan42@gmail.com>
2020-08-17 12:01:19 -0500

    Moved Files to Mod2 Folder
    Moved Files to Mod2 Folder

---
+++ db/migrate/20200817164924_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.integer :age
+      t.string :skill_level
+      t.string :address
+
+      t.timestamps
+    end
+  end
+end
